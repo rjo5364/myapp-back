35c06daf839c8e517abc06eb5f954fdafef289e2
Rafael Orozco <rjo5364@psu.edu>
2025-01-29 15:24:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -91,8 +91,8 @@ app.use(
       secure: true,
       sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
-      domain: process.env.COOKIE_DOMAIN,
-      path: '/'
+     // domain: process.env.COOKIE_DOMAIN,
+     // path: '/'
     }
   })
 );
